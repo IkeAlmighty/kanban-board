e832513587792470e9e7bde35fad05a110f8e6d1
ikealmighty <isaacyates7@gmail.com>
2025-03-26 14:49:31 -0500

    added 500 response handling

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -7,20 +7,24 @@ interface AuthTokenData {
 const login = async (userInfo: UserLogin): Promise<AuthTokenData> => {
   // make a POST request to the login route
   const { username, password } = userInfo;
+  const body = JSON.stringify({ username, password })
 
   const response = await fetch("/auth/login", {
     method: "POST",
-    body: JSON.stringify({ username, password }),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body,
   });
 
   if (response.ok) {
     // return the token
     const { authToken } = await response.json();
     return { token: authToken };
-  } else {
+  } else if (response.status !== 500) {
     const { error } = await response.json();
     throw new Error(error);
-  }
+  } else throw new Error('Internal Server Error')
 };
 
 export { login };
